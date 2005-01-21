3934fe9bdc66cb3c59a015f46ddfe4dc6fb624ce
antoine <antoine@b819a8a0-03e9-0310-83c9-e64e4d513ffc>
2005-01-21 13:35:25 +0000

    copyrigth

--- trunk/main/solipsis/services/chat/network.py
+++ trunk/main/solipsis/services/chat/network.py
@@ -1,3 +1,21 @@
+# <copyright>
+# Solipsis, a peer-to-peer serverless virtual world.
+# Copyright (C) 2002-2005 France Telecom R&D
+# 
+# This software is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+# 
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this software; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# </copyright>
 
 
 from twisted.internet.protocol import DatagramProtocol
@@ -35,10 +53,9 @@ class NetworkLauncher(object):
             self.Stop()
         self.protocol = Protocol()
         self.listening = self.reactor.listenUDP(self.port, self.protocol)
-        # For test purposes
+        # For test purposes, chat with ourselves ;)
         host = self.listening.getHost().host
         self.protocol.SetHosts([(host, self.port)])
-        self.protocol.SendMessage("ha! ha! ha!")
 
     def Stop(self):
         self.listening.stopListening()
