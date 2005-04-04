b7652b528999ad8d6cb5a94ca4f9eb3a34a5df2d
emb <emb@b819a8a0-03e9-0310-83c9-e64e4d513ffc>
2005-04-04 15:27:03 +0000

    implemented sharing system

---
+++ trunk/main/solipsis/services/profile/data.py
@@ -0,0 +1,164 @@
+# <copyright>
+# Solipsis, a peer-to-peer serverless virtual world.
+# Copyright (C) 2002-2005 France Telecom R&D
+# 
+# This software is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+# 
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this software; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# </copyright>
+"""Define cache structures used in profile and rendered in list widgets"""
+
+from os.path import isdir, isfile
+import os
+
+DEFAULT_TAG = u"none"
+
+class FileContainer:
+    """Structure to store files info in cache"""
+
+    def __init__(self, path, name, share=True, tag=DEFAULT_TAG):
+        if not isfile(path):
+            raise ValueError("%s not a valid file"% name)
+        self.path = path
+        self.name = name
+        self._tag = tag
+        self.shared = share
+
+    def __str__(self):
+        return "%s%s"% (self.name, self.shared and " [shared]" or "")
+
+    def __repr__(self):
+        return "%s %s [%s]"% (self.path, self.shared and "[shared]" or "", self._tag)
+
+    def tag(self, tag):
+        """set tag"""
+        self._tag = tag
+
+    def share(self, share=True):
+        """set sharing status"""
+        self.shared = share
+        
+class DirContainer:
+    """Structure to store data in cache:
+    
+    [item, name, #shared, [data to display on right side]"""
+
+    def __init__(self, path, item=None):
+        if not isdir(path):
+            raise ValueError("%s not a valid dir"% path)
+        self.path = path
+        self.name = path.split(os.path.sep)[-1]
+        self.item = item
+        self.content = {}
+        self.share_content(False)
+
+    def __str__(self):
+        return "%s [%d]"% (self.name, self.nb_shared())
+
+    def __repr__(self):
+        return "%s %s [%d]"% (self.name, str(self.content), self.nb_shared())
+
+    def share_content(self, share=True):
+        """(un)share all files of directory"""
+        self.share_files([name for name in os.listdir(self.path)
+                          if isfile(os.path.join(self.path, name))],
+                         share)
+        
+    def share_files(self, names, share=True):
+        """share given files of dir"""
+        for name in names:
+            if not self.content.has_key(name):
+                self.content[name] = FileContainer(os.path.join(self.path, name),
+                                              name, share)
+            else:
+                self.content[name].share(share)
+
+    def tag_files(self, names, tag):
+        """modify tag of shared file. Share file if not"""
+        for name in names:
+            if not self.content.has_key(name):
+                self.content[name] = FileContainer(os.path.join(self.path, name),
+                                              name, False, tag)
+            else:
+                self.content[name].tag(tag)
+
+    def nb_shared(self):
+        """return number of shared element"""
+        result = 0
+        for file_container in self.content.values():
+            if file_container.shared:
+                result += 1
+        return result
+
+class SharingContainer:
+    """stores all DirContainer along with items"""
+
+    def __init__(self, adding_cb, deleting_cb, display_cb):
+        self.data = {}
+        self.add_to_view = adding_cb
+        self.del_from_view = deleting_cb
+        self.display = display_cb
+
+    def add_repository(self, full_path, root_item):
+        """add shared directory to list"""
+        self._add_dir(full_path, root_item)
+
+    def remove_repository(self, full_path):
+        """add shared directory to list"""
+        dir_container = self.data[full_path]
+        # remove from widget
+        self.del_from_view(self.data[full_path].item)
+        # remove from cache
+        del self.data[full_path]
+
+    def expand_dir(self, full_path):
+        """put into cache new information when dir expanded in tree"""
+        dir_container = self.data[full_path]
+        # add each dir of browsed dir
+        for dir_name in [os.path.join(dir_container.path, name) for name in os.listdir(dir_container.path)]:
+            if isdir(dir_name):
+                self._add_dir(dir_name, dir_container.item)
+            else:
+                # not a dir, do nothing
+                pass
+
+    def share_dir(self, full_path, share=True):
+        """forward command to cache"""
+        container = self.data[full_path]
+        container.share_content(share)
+        self.display(container)
+
+    def share_files(self, full_path, names, share=True):
+        """forward command to cache"""
+        container = self.data[full_path]
+        container.share_files(names, share)
+        self.display(container)
+
+    def tag_files(self, full_path, names, tag):
+        """forward command to cache"""
+        self.data[full_path].tag_files(names, tag)
+        
+    def get_dir_content(self, full_path):
+        """return data assocaited with a DirContainer"""
+        return self.data[full_path].content
+
+    def _add_dir(self, full_path, parent_item):
+        """add shared directory to list"""
+        dir_container = DirContainer(full_path)
+        # add in widget
+        child_item = self.add_to_view(parent_item, dir_container.name)
+        dir_container.item = child_item
+        # add in cache
+        self.data[full_path] = dir_container
+        # display result
+        self.display(dir_container)
---
+++ trunk/main/solipsis/services/profile/tests/unittest_data.py
@@ -0,0 +1,114 @@
+"""Represents data stored in cache, especially shared file information"""
+
+import unittest
+import sys
+
+from StringIO import StringIO
+from solipsis.services.profile.data import SharingContainer, DEFAULT_TAG
+
+class SimpleView:
+    """utility class to emulate real widget (wx.ListCtrl)"""
+    def __init__(self, iostring):
+        self.content = {}
+        self.iostring = iostring
+        self.item = 0
+
+    def __str__(self):
+        return self.iostring.getvalue()
+
+    def append_item(self, parent, name):
+        self.item += 1
+        self.content[self.item] = name
+        return self.item
+
+    def delete_item(self, item):
+        del self.content[item]
+
+    def display(self, container):
+        print >> self.iostring, container
+
+
+class CacheTest(unittest.TestCase):
+    """test that cache coherency with following arborescency
+         data
+         data/routage
+         data/date.txt
+         data/.path
+         data/emptydir
+         data/subdir1
+         data/subdir1/date.doc
+         data/subdir1/subsubdir
+         data/subdir1/subsubdir/default.solipsis
+         data/subdir1/subsubdir/dummy.txt
+         data/subdir1/subsubdir/null"""
+        
+    def setUp(self):
+        """override one in unittest.TestCase"""
+        self.view = SimpleView(StringIO())
+#         self.view = SimpleView(sys.stdout)
+        self.container = SharingContainer(self.view.append_item,
+                                          self.view.delete_item,
+                                          self.view.display)
+
+    def test_repository(self):
+        """add and remove repo"""
+        self.container.add_repository("data", 0)
+        self.container.add_repository("data/subdir1/subsubdir", 0)
+        self.container.add_repository("data/emptydir", 0)
+        self.assert_(self.container.data.has_key("data"))
+        self.assert_(self.container.data.has_key("data/subdir1/subsubdir"))
+        self.assert_(self.container.data.has_key("data/emptydir"))
+        
+        self.container.remove_repository("data/subdir1/subsubdir")
+        self.container.remove_repository("data")
+        self.assert_(not self.container.data.has_key("data"))
+        self.assert_(not self.container.data.has_key("data/subdir1/subsubdir"))
+        self.assert_(self.container.data.has_key("data/emptydir"))
+
+    def test_expanding(self):
+        """good completion of dirs on expanding command"""
+        self.container.add_repository("data", 0)
+        self.container.expand_dir("data")
+        self.assert_(self.container.data.has_key("data"))
+        self.assert_(self.container.data.has_key("data/subdir1"))
+        self.assert_(self.container.data.has_key("data/emptydir"))
+        self.assert_(not self.container.data.has_key("data/subdir1/subsubdir"))
+        self.assert_(not self.container.data.has_key("data/routage"))
+        self.assert_(not self.container.data.has_key("data/.path"))
+        self.assert_(not self.container.data.has_key("data/date.txt"))
+        
+    def test_sharing(self):
+        """share/unshare file and dirs"""
+        self.container.add_repository("data", 0)
+        self.container.expand_dir("data")
+        self.container.expand_dir("data/subdir1")
+        self.assertEquals(self.container.data["data"].nb_shared(), 0)
+        self.container.share_files("data", [".path", "date.txt"])
+        self.assertEquals(self.container.data["data"].nb_shared(), 2)
+        self.container.share_dir("data/subdir1/subsubdir")
+        self.assertEquals(self.container.data["data/subdir1/subsubdir"].nb_shared(), 3)
+        self.container.share_dir("data", False)
+        self.assertEquals(self.container.data["data"].nb_shared(), 0)
+        self.container.share_files("data/subdir1/subsubdir", ["dummy.txt"], False)
+        self.assertEquals(self.container.data["data/subdir1/subsubdir"].nb_shared(), 2)
+
+    def test_tagging(self):
+        """coherent listing of content"""
+        self.container.add_repository("data", 0)
+        self.container.tag_files("data", ["routage", "date.txt"], u"tag1")
+        self.assertEquals(self.container.data["data"].content["routage"]._tag, u"tag1")
+        self.assertEquals(self.container.data["data"].content["date.txt"]._tag, u"tag1")
+        self.assertEquals(self.container.data["data"].content[".path"]._tag, DEFAULT_TAG)
+        self.assertRaises(ValueError, self.container.tag_files, *("data", ["subdir1"], u"tag1"))
+
+    def test_listing(self):
+        """coherent listing of content"""
+        self.container.add_repository("data", 0)
+        self.container.expand_dir("data")
+        self.container.share_files("data", [".path", "date.txt"])
+        self.container.tag_files("data", ["routage", "date.txt"], u"tag1")
+        content = self.container.get_dir_content("data")
+        self.assertEquals(str(content), "{'routage': data/routage  [tag1], '.path': data/.path [shared] [none], 'date.txt': data/date.txt [shared] [tag1]}")
+    
+if __name__ == '__main__':
+    unittest.main()
