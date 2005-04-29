75c500a7824ae6e13def97c814cd21c114e27a72
emb <emb@b819a8a0-03e9-0310-83c9-e64e4d513ffc>
2005-04-29 16:08:40 +0000

    added tcp-driven navigator for test purpose

---
+++ trunk/main/netNavigator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import os,sys
+from solipsis.navigator.cmdclient.main import main
+
+# add current directory to the python search path
+# it is needed in case solipsis is not installed in the standard python
+# directory (e.g: /usr/lib/python2.3 in linux)
+
+currentDir = sys.path[0]
+if os.environ.has_key('PYTHONPATH'):
+    os.environ['PYTHONPATH'] = os.environ['PYTHONPATH'] + ':' + currentDir
+else:
+    os.environ['PYTHONPATH'] = currentDir
+
+if __name__ == '__main__':
+    main()
