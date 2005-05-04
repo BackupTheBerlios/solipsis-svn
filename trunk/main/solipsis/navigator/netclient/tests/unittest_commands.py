67ea3de5a2fb634995009f3c8b7c3cc11f15c088
emb <emb@b819a8a0-03e9-0310-83c9-e64e4d513ffc>
2005-05-04 16:38:38 +0000

    added structure of unittests

---
+++ trunk/main/solipsis/navigator/netclient/tests/unittest_commands.py
@@ -0,0 +1,18 @@
+"""Represents data stored in cache, especially shared file information"""
+
+import unittest
+import sys
+
+
+class CommandTest(unittest.TestCase):
+    """"""
+        
+    def setUp(self):
+        """override one in unittest.TestCase"""
+        self.container = DirContainer(REPO)
+
+    def test_about(self):
+        """print information about..."""
+        
+if __name__ == '__main__':
+    unittest.main()
