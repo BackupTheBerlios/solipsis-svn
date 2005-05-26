ceb12691d0bf2c974a5d413566516c8608350f00
antoine <antoine@b819a8a0-03e9-0310-83c9-e64e4d513ffc>
2005-05-26 11:45:30 +0000

    official version number

--- branches/release-0.8.3/main/solipsis/navigator/wxclient/app.py
+++ branches/release-0.8.3/main/solipsis/navigator/wxclient/app.py
@@ -54,7 +54,7 @@ class NavigatorApp(wx.App, XRCLoader, UIProxyReceiver):
     """
     Main application class. Derived from wxPython "wx.App".
     """
-    version = "0.8.3svn"
+    version = "0.8.3"
     config_file = os.sep.join(["state", "config.bin"])
 
     def __init__(self, params, *args, **kargs):
