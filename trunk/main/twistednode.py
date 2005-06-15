0f86e80a9175b9d73a96e7a0773e12b4b8a025f1
antoine <antoine@b819a8a0-03e9-0310-83c9-e64e4d513ffc>
2005-06-15 15:01:27 +0000

    same path management in both executables + do not duplicate current dir if already in sys.path

--- trunk/main/twistednode.py
+++ trunk/main/twistednode.py
@@ -5,7 +5,12 @@ import os.path
 import sys
 
 if __name__ == '__main__':
-    sys.path.insert(0, os.path.normcase(os.path.dirname(os.path.abspath(sys.argv[0]))))
-    #print sys.path
+    # Adjust path
+    current_file = sys.argv[0]
+    current_path = os.path.normcase(os.path.dirname(os.path.abspath(current_file)))
+    os.chdir(current_path)
+    if not current_path in sys.path:
+        sys.path.insert(0, current_path)
+    # Launch
     from solipsis.node import main
     main.main()
