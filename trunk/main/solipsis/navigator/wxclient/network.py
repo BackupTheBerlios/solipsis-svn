98f2cc7dfabec2b26d1ec85ab3e671d4e71f9e96
emb <emb@b819a8a0-03e9-0310-83c9-e64e4d513ffc>
2005-07-27 15:19:01 +0000

    last commit a little too fast

--- trunk/main/solipsis/navigator/wxclient/network.py
+++ trunk/main/solipsis/navigator/wxclient/network.py
@@ -3,9 +3,6 @@
 # Copyright (C) 2002-2005 France Telecom R&D
 # 
 # This software is free software; you can redistribute it and/or
-# pylint: disable-msg=C0101,C0103
-# Too short name // Invalid name
-#
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
 # version 2.1 of the License, or (at your option) any later version.
@@ -20,23 +17,23 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 # </copyright>
 
-from twisted.internet import protocol
+from pprint import pprint
+from cStringIO import StringIO
+
+from twisted.internet import protocol, reactor, defer
 from twisted.protocols import basic
+from solipsis.navigator.validators import AddressValidator
+from solipsis.navigator.network import BaseNetworkLoop
+from solipsis.util.uiproxy import UIProxy
 
-class URLListenProtocol(basic.LineReceiver):
-    """see twisted for more details on the protocol/factory model"""
-    def lineReceived(self, url):
-        """exoected lines are url"""
-        self.factory.ProcessURL(url)
-        self.transport.loseConnection()
-
-class URLListenFactory(protocol.ServerFactory):
-    """wraps ui (NavigatorApp) """
-    protocol = URLListenProtocol
-
-    def __init__(self, ui):
-        self.ui = ui
-
-    def ProcessURL(self, url):
-        """forward command to ui"""
-        self.ui._JumpNearURL(url)
+class NetworkLoop(BaseNetworkLoop):
+    """
+    NetworkLoop is an event loop running in its own thread.
+    It manages socket-based communication with the Solipsis node
+    and event-based communication with the User Interface (UI).
+    """
+    def __init__(self, reactor, ui):
+        """
+        Builds a network loop from a Twisted reactor and a Wx event handler.
+        """
+        BaseNetworkLoop.__init__(self, reactor, UIProxy(ui))
